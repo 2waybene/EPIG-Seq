22cd2db9e1f7842d6c7b1e7914bf30997cd20623
2waybene <jianying.li@gmail.com>
2015-04-20 10:25:53 -0400

    updated_04202015

--- scriptDir/compute_confusion_matrix.R
+++ scriptDir/compute_confusion_matrix.R
@@ -12,3 +12,26 @@ dt[grep ("_C_", dt$ID),2]
 dt[grep ("_D_", dt$ID),2]
 dt[grep ("_E_", dt$ID),2]
 
+summary(as.factor(dt[grep ("_A_", dt$ID),2]))
+summary(as.factor(dt[grep ("_B_", dt$ID),2]))
+summary(as.factor(dt[grep ("_C_", dt$ID),2]))
+summary(as.factor(dt[grep ("_D_", dt$ID),2]))
+summary(as.factor(dt[grep ("_E_", dt$ID),2]))
+
+
+#pattern2 = _A_
+#pattern3 = _B_
+#pattern1 = _C_
+#pattern5 = _D_
+#pattern4 = _E_
+
+
+file <- "X:/myGit/EPIG-Seq/testRuns/Categorized_simulated_EPIG-Seq_data_6_patterns_final_F_group_randomized_new_with_mapped_reads_corr_09_08_PCT_5_St_5_3.5_fixed_correctly_betav02_5.txt"
+count.fields (file = file, sep ="\t") [1:10]
+dt <- read.table (file, header = TRUE,comment.char = "",sep = "\t")
+
+summary(as.factor(dt[grep ("_A_", dt$ID),2]))
+summary(as.factor(dt[grep ("_B_", dt$ID),2]))
+summary(as.factor(dt[grep ("_C_", dt$ID),2]))
+summary(as.factor(dt[grep ("_D_", dt$ID),2]))
+summary(as.factor(dt[grep ("_E_", dt$ID),2]))
